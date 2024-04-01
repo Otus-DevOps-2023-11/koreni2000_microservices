edb2f8cd8734e56ac4d09c3525b15ff0326570de
Андрей Шульга <koreni2000@gmail.com>
2024-04-01 20:52:39 +0300

    HW  logging-1

--- src/ui/docker_build.sh
+++ src/ui/docker_build.sh
@@ -2,5 +2,5 @@
 
 echo `git show --format="%h" HEAD | head -1` > build_info.txt
 echo `git rev-parse --abbrev-ref HEAD` >> build_info.txt
-cd "$(dirname "$0")" || exit
+
 docker build -t $USER_NAME/ui .
